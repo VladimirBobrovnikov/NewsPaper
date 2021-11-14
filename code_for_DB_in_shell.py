53b3d7ab42747fc01315d246e72c651430809dd4
Bobrovnikov Vladimir <t9126362257@gmail.com>
2021-11-14 14:07:17 +0500

    first commit

---
+++ code_for_DB_in_shell.py
@@ -0,0 +1,53 @@
+from news.models import *
+
+
+User.objects.create_user('user1')
+User.objects.create_user('user2')
+aut1 = Author(user=User.objects.get(pk=1))
+aut1.save()
+aut2 = Author(user=User.objects.get(pk=2))
+aut2.save()
+Category.objects.create(name='Фантастика')
+Category.objects.create(name='Учебный материал')
+Category.objects.create(name='Художественное повествование')
+Category.objects.create(name='Научная статья')
+title = 'Вымирание смурфиков неизбежно'
+text = 'Максимально аргумментированный текст про то, что все смурфики погибнут'
+pos1 = Post(
+    author=Author.objects.get(pk=1),
+    post_or_news='POST',
+    title=title,
+    text=text
+)
+pos1.save()
+pos2 = Post(author=aut1, post_or_news='POST', title='Python в современном мире', text='Рассказ о питоне с примерами кода')
+pos2.save()
+news1 = Post(author=aut2, post_or_news='NEWS', title='Объявление об отключении интернета', text='Даты и сроки отключения с извиненеиями')
+news1.save()
+PostCategory.objects.create(post=pos1, category=Category.objects.get(pk=1))
+PostCategory.objects.create(post=pos2, category=Category.objects.get(pk=2))
+PostCategory.objects.create(post=pos2, category=Category.objects.get(pk=4))
+PostCategory.objects.create(post=news1, category=Category.objects.get(pk=1))
+User.objects.create_user(username='user3')
+Comment.objects.create(post=pos1, user=User.objects.get(pk=3), text='Я считаю это глупостями')
+Comment.objects.create(post=pos2, user=User.objects.get(pk=2), text='коммент')
+Comment.objects.create(post=pos1, user=User.objects.get(pk=2), text='коммент2')
+Comment.objects.create(post=news1, user=User.objects.get(pk=1), text='Неновижу отключения')
+pos1.like()
+pos2.dislike()
+news1.like()
+Comment.objects.get(pk=1).like()
+Comment.objects.get(pk=2).like()
+Comment.objects.get(pk=3).like()
+Comment.objects.get(pk=4).dislike()
+aut1.update_rating()
+aut2.update_rating()
+first = Author.objects.all().order_by('-rating_author').values('user__username', 'rating_author').first()
+best_post = Post.objects.all().order_by('-rating_post').first()
+Post.objects.all().order_by('-rating_post').values('date_time_in', 'author__user__username', 'rating_post', 'title').first()
+best_post.preview()
+values_b_p = [_.values() for _ in Post.objects.all().order_by('-rating_post').values('date_time_in', 'author__user__username', 'rating_post', 'title').first()]
+comments_best_posts = Comment.objects.filter(post=best_post).values('date_time', 'user__username', 'rating_comment', 'text')
+values_c_b_p = [_.values() for _ in comments_best_posts]
+
+
---
+++ news/models.py
@@ -0,0 +1,71 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+CHOICES_POST_NEWS = [
+    ('POST', 'Статья'),
+    ('NEWS', 'Новость')
+]
+
+
+class Author(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating_author = models.IntegerField(default=0)
+
+    def update_rating(self):
+        summ_rating_posts = 0  # суммарный рейтинг каждой статьи автора;
+        summ_rating_comments_at_post = 0  # суммарный рейтинг всех комментариев к статьям автора;
+        summ_rating_comments = 0  # суммарный рейтинг всех комментариев автора;
+        for element in self.posts.all():
+            summ_rating_posts += int(element.rating_post)
+            for _ in element.comments_p.all():
+                summ_rating_comments_at_post += int(_.rating_comment)
+        for element in self.user.comments_u.all():
+            summ_rating_comments += int(element.rating_comment)
+        self.rating_author = (3 * summ_rating_posts) + summ_rating_comments + summ_rating_comments_at_post
+        self.save()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
+class Post(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='posts')
+    post_or_news = models.CharField(max_length=4, choices=CHOICES_POST_NEWS, default='POST')
+    date_time_in = models.DateTimeField(auto_now_add=True)
+    category = models.ManyToManyField(Category, through='PostCategory')
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    rating_post = models.IntegerField(default=0)
+
+    def like(self):
+        self.rating_post += 1
+        self.save()
+
+    def dislike(self):
+        self.rating_post -= 1
+        self.save()
+
+    def preview(self):
+        return (self.text[:124] + '...')
+
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments_p')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments_u')
+    text = models.TextField()
+    date_time = models.DateTimeField(auto_now_add=True)
+    rating_comment = models.IntegerField(default=0)
+
+    def like(self):
+        self.rating_comment += 1
+        self.save()
+
+    def dislike(self):
+        self.rating_comment -= 1
+        self.save()
